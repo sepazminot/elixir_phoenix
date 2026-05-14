02cfe0263a5e2f8f042e98394361f2fd8d7c0d88
PAZMIÑO TORRES SERGIO EDWIN <sepazminot@utn.edu.ec>
2026-05-14 01:08:40 -0500

    Activar servidor

--- config/runtime.exs
+++ config/runtime.exs
@@ -58,8 +58,8 @@ if config_env() == :prod do
   config :elixir_phoenix, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
   port = String.to_integer(System.get_env("PORT") || "4000")
   config :elixir_phoenix, ElixirPhoenixWeb.Endpoint,
-
-    url: [host: System.get_env("PHX_HOST") || "example.com", port: 443, scheme: "https"],
+    server: true,
+    url: [host: host, port: 443, scheme: "https"],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
