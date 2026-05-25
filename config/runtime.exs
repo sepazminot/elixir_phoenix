49d9ee08d44e03d3a8f3f36cec38f66bb3ef4faa
PAZMIÑO TORRES SERGIO EDWIN <sepazminot@utn.edu.ec>
2026-05-24 23:57:28 -0500

    Quitando config

--- config/runtime.exs
+++ config/runtime.exs
@@ -18,7 +18,6 @@ import Config
 # script that automatically sets the env var above.
 if System.get_env("PHX_SERVER") do
   config :elixir_phoenix, ElixirPhoenixWeb.Endpoint,
-  cache_static_manifest: "priv/static/cache_manifest.json",
   server: true
 end
 
